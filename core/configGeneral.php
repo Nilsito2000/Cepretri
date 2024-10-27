2d654722c36a08bc838337e12b2c9bd60d3b0283
unknown <nileduardo2000@gmail.com>
2024-10-27 11:38:29 -0500

    Cepetri

---
+++ core/configGeneral.php
@@ -0,0 +1,9 @@
+<?php
+	const SERVERURL = "http://localhost/cepetri/";
+
+	const COMPANY = "CEPETRI";
+
+	/*====================================
+	=            Zona horaria            =
+	====================================*/
+	date_default_timezone_set("America/Lima");
